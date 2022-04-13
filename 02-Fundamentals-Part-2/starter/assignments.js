beacf0afb931301533f0553617f65f1952e70f23
Agnimitra Pathak <agnimitra.vr@gmail.com>
2022-04-13 13:54:37 +0530

    Last commit on this assignment

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -4,38 +4,38 @@ function c(value) {
   console.log(value);
 }
 
-let getLicense = false;
-const driverLicense = true;
+// let getLicense = false;
+// const driverLicense = true;
 
-if (driverLicense) getLicense = true;
-// if (getLicense) c("Drive, the shit!!");
+// if (driverLicense) getLicense = true;
+// // if (getLicense) c("Drive, the shit!!");
 
-// Assingnment #1 – Functions
-//#region
-function describeCountry(country, population, capitalCity) {
-  return `${country} has ${population} million people and it's capital city is ${capitalCity}`;
-}
-const message = describeCountry("India", 130, "Delhi");
+// // Assingnment #1 – Functions
+// //#region
+// function describeCountry(country, population, capitalCity) {
+//   return `${country} has ${population} million people and it's capital city is ${capitalCity}`;
+// }
+// const message = describeCountry("India", 130, "Delhi");
 // c(message);
 
-//#endregion
+// #endregion
 
 // Assingnment #2 – Functions as declarations and expressions. The difference is in calling the function. It behvaes becasue of the concept called hoisting.
-//#region
+// #region
 
 // Function as declaration
-function percentageOfWorld1(population) {
-  return (population / 7900) * 100;
-}
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
 
-const percentage = percentageOfWorld1(1330);
-// c(percentage);
+// const percentage = percentageOfWorld1(1330);
+// // c(percentage);
 
-// Function as an expression
+// // Function as an expression
 
-const percentageOfWorld2 = function (population) {
-  return (population / 7900) * 100;
-};
+// const percentageOfWorld2 = function (population) {
+//   return (population / 7900) * 100;
+// };
 // c(percentageOfWorld2(1441));
 //#endregion
 
@@ -64,5 +64,85 @@ const describePopulation = (country, population) => {
 // c(describePopulation("India", 1330));
 // c(describePopulation("China", 1441));
 // c(describePopulation("Finland", 13));
+
+//#endregion
+
+// Assingnment #5 – Arrays
+//#region
+// const populations = [13, 1441, 1330, 33];
+// if (populations.length === 4) {
+//   // c(true);
+// } else {
+//   // c(false);
+// }
+// const percentages = [
+//   percentageOfWorld1(populations[0]),
+//   percentageOfWorld1(populations[1]),
+//   percentageOfWorld1(populations[2]),
+//   percentageOfWorld1(populations[3]),
+// ];
+
+// c(percentages);
+
+//#endregion
+
+// Assingnment #5 – Array methods
+//#region
+const neighbours = ["Mumbai", "Delhi", "Bangalore", "Hyderabad", "Chennai"];
+
+neighbours.push("Utopia");
+// c(neighbours);
+
+neighbours.pop();
+// c(neighbours);
+
+const checkCountry = neighbours.includes("Gernmany");
+// c(checkCountry);
+
+const whereIs = neighbours.indexOf("Hyderabad");
+// c(whereIs);
+
+neighbours[whereIs] = "Hey the ra bad";
+// c(neighbours);
+
+//#endregion
+
+// Assingnment #6 – Objects
+//#region
+// const myCountry = {
+//   country: "India",
+//   capital: "Delhi",
+//   language: "Hindi",
+//   population: 133,
+//   neighbours: ["Bhutan", "Nepal", "China", "Sri Lanka", "Pakistan"],
+
+//   describe: function () {
+//     return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`;
+//   },
+// };
+// console.log(myCountry.describe());
+// //#endregion
+
+// // Assingnment #6 – Object notations
+// //#region
+
+// // console.log(
+// //   `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
+// // );
+
+// myCountry.population = myCountry.population + 2;
+// // console.log(myCountry.population);
+
+// myCountry["population"] = myCountry["population"] - 2;
+// console.log(myCountry["population"]);
+//#endregion
+
+// Assingnment #7 – Loops
+
+//#region
+
+for (let num = 1; num <= 50; num++) {
+  console.log(`Voter number ${num} is currently voting.`);
+}
 
 //#endregion
--- 02-Fundamentals-Part-2/starter/codechallenge.js
+++ 02-Fundamentals-Part-2/starter/codechallenge.js
@@ -5,45 +5,110 @@ function c(value) {
 // Code challenge #1
 //#region
 
-//Test data
-const dolphineScore1 = Number(prompt("Dolphine score 1"));
-c(dolphineScore1);
-const dolphineScore2 = Number(prompt("Dolphine score 2"));
-c(dolphineScore2);
-const dolphineScore3 = Number(prompt("Dolphine score 3"));
-c(dolphineScore3);
+// // Test data
+// const dolphineScore1 = Number(prompt("Dolphine score 1"));
+// c(dolphineScore1);
+// const dolphineScore2 = Number(prompt("Dolphine score 2"));
+// c(dolphineScore2);
+// const dolphineScore3 = Number(prompt("Dolphine score 3"));
+// c(dolphineScore3);
 
-const koalaScore1 = Number(prompt("Koalas score 1"));
-c(koalaScore1);
-const koalaScore2 = Number(prompt("Koalas score 2"));
-c(koalaScore2);
-const koalaScore3 = Number(prompt("Koalas score 3"));
-c(koalaScore3);
+// const koalaScore1 = Number(prompt("Koalas score 1"));
+// c(koalaScore1);
+// const koalaScore2 = Number(prompt("Koalas score 2"));
+// c(koalaScore2);
+// const koalaScore3 = Number(prompt("Koalas score 3"));
+// c(koalaScore3);
 
-//Function
-const calcAverage = (Score1, Score2, Score3) => (Score1 + Score2 + Score3) / 3;
+// //Function
+// const calcAverage = (Score1, Score2, Score3) => (Score1 + Score2 + Score3) / 3;
 
-const avgDolphine = calcAverage(dolphineScore1, dolphineScore2, dolphineScore3);
-const avgKoala = calcAverage(koalaScore1, koalaScore2, koalaScore3);
+// const avgDolphine = calcAverage(dolphineScore1, dolphineScore2, dolphineScore3);
+// const avgKoala = calcAverage(koalaScore1, koalaScore2, koalaScore3);
 
-c(`Average score of Dolphine is ${avgDolphine}`);
-c(`Average score of Koala is ${avgKoala}`);
+// // c(`Average score of Dolphine is ${avgDolphine}`);
+// // c(`Average score of Koala is ${avgKoala}`);
 
-const checkWinner = (value1, value2) => {
-  if (value1 >= 2 * value2) {
-    c(`Dolphine wins by ${value1} vs. ${value2}`);
-    return value1;
-  } else if (value2 >= 2 * value1) {
-    c(`Koala wins by ${value2} vs. ${value1}`);
-    return value2;
-  } else if (value1 === value2) {
-    c(`It's a tie ${value1} vs. ${value2}`);
-    return value1;
-  } else {
-    c("Nobody won");
-    return -1;
-  }
-};
+// const checkWinner = (value1, value2) => {
+//   if (value1 >= 2 * value2) {
+//     // c(`Dolphine wins by ${value1} vs. ${value2}`);
+//     return value1;
+//   } else if (value2 >= 2 * value1) {
+//     // c(`Koala wins by ${value2} vs. ${value1}`);
+//     return value2;
+//   } else if (value1 === value2) {
+//     // c(`It's a tie ${value1} vs. ${value2}`);
+//     return value1;
+//   } else {
+//     // c("Nobody won");
+//     return -1;
+//   }
+// };
 
-checkWinner(avgDolphine, avgKoala);
+// checkWinner(avgDolphine, avgKoala);
+//#endregion
+
+// Code challenge #2
+//#region
+
+// const bills = [
+//   Number(prompt("Add bill amount 1")),
+//   Number(prompt("Add bill amount 2")),
+//   Number(prompt("Add bill amount 3")),
+// ];
+// c(bills);
+
+// const calcTip = (billValue) => {
+//   if (50 <= billValue && billValue <= 300) {
+//     return billValue * 0.15;
+//   } else {
+//     return billValue * 0.2;
+//   }
+// };
+
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// c(tips);
+
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+// console.log(bills, tips, totals);
+//#endregion
+
+// Code challlenge #3
+//#region
+
+// const mark = {
+//   fullName: "Mark Miller",
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     this.BMI = this.mass / this.height ** 2;
+//     return this.BMI;
+//   },
+// };
+
+// const john = {
+//   fullName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     this.BMI = this.mass / this.height ** 2;
+//     return this.BMI;
+//   },
+// };
+
+// mark.calcBMI();
+// john.calcBMI();
+// // console.log(john.BMI, mark.BMI);
+
+// if (mark.BMI > john.BMI) {
+//   console.log(
+//     `${mark.fullName}'s BMI (${mark.BMI}) is higher than ${john.fullName}'s BMI (${john.BMI})`
+//   );
+// } else if (mark.BMI < john.BMI) {
+//   console.log(
+//     `${john.fullName}'s BMI (${john.BMI}) is higher than ${mark.fullName}'s BMI (${mark.BMI})`
+//   );
+// }
+
 //#endregion
